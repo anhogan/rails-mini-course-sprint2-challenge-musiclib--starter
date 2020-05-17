033934b8239cd062a4832af2261bdb9c105273f4
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-05-17 12:40:00 -0400

    Write RSpec tests

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -3,32 +3,62 @@ require 'rails_helper'
 RSpec.describe Album, type: :model do
   describe "validations" do
     it "is valid" do
-      # instantiate a valid album and ensure it is valid
+      album = Album.new(name: 'My First Album')
+
+      result = album.valid?
+      errors = album.errors.full_messages
+
+      expect(result).to be true
+      expect(errors.count).to be 0
     end
 
     it "is invalid without a name" do
-      # instantiate an album without a name and ensure it is invalid
+      album = Album.new(name: nil)
+
+      result = album.valid?
+      errors = album.errors.full_messages
+
+      expect(result).to be false
+      expect(errors.count).to be > 0
     end
   end
 
   describe "attributes" do
     it "has expected attributes" do
-      # ensure exactly the expected attributes are present on an album
+      attributes = Album.new(name: 'My First Album').attribute_names
+
+      expect(attributes).to contain_exactly("id", "name", "available", "artist_id", "created_at", "updated_at")
     end
   end
 
   context "scopes" do
     describe "available" do
+      before do
+        Album.create!( { name: "First", available: true } )
+        Album.create!( { name: "Second", available: false } )
+        Album.create!( { name: "Third", available: true } )
+        Album.create!( { name: "Fourth", available: true } )
+        Album.create!( { name: "Fifth", available: false } )
+      end
+
       it "returns a list of available albums sorted by name" do
-        # set up a some available albums and unavailable albums and make expecations that the
-        # available albums scope only returns available albums sorted by name
+        results = Album.available
+
+        expect(results.count).to eq 3
+        expect(results.first.name).to eq "First"
+        expect(results.last.name).to eq "Fourth"
       end
     end
   end
 
   describe "#length_seconds" do
     it "calculates the total length in seconds of an album" do
-      # setup a valid album and songs and make expecations about the return value of length seconds
+      album = Album.new(name: 'My First Album', id: 1)
+      songOne = Song.new(title: 'Beginning', length_seconds: 180, track_number: 1, album_id: 1)
+      songTwo = Song.new(title: 'Middle', length_seconds: 200, track_number: 2, album_id: 1)
+      songThree = Song.new(title: 'Eng', length_seconds: 220, track_number: 3, album_id: 1)
+
+      expect(album).to eq 600
     end
   end
 end
--- spec/requests/api/v1/albums/get_album_spec.rb
+++ spec/requests/api/v1/albums/get_album_spec.rb
@@ -3,15 +3,18 @@ require 'rails_helper'
 RSpec.describe "Get Album", type: :request do
   describe "GET /api/v1/albums/:id" do
     let(:album) {
-      # set up an album to get
+      Album.create!(name: 'My First Album', available: true)
     }
 
     it "gets the album" do
       get api_v1_album_path(album)
       json_body = JSON.parse(response.body).deep_symbolize_keys
 
-      # write an expectation about the response status code
-      # write an expecation about the response json_body
+      expect(response).to have_http_status(200)
+      expect(response_body).to include({
+        name: "My First Album",
+        available: true
+      })
     end
   end
 end
